7d6288740ee9f8312a6a9897392dd6689bfcc526
spite <the.spite@gmail.com>
2014-02-19 21:44:36 +0100

    added average of values added css loading fixed performance issue added custom colour feature to graphs

--- src/rStats.js
+++ src/rStats.js
@@ -28,54 +28,68 @@ function rStats( settings ) {
 
     'use strict';
 
-    var element = document.createElement('link');
-    element.href = 'http://fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300';
-    element.rel = 'stylesheet';
-    element.type = 'text/css';
-    document.getElementsByTagName('head')[0].appendChild(element)
+    function importCSS( url ){
+
+        var element = document.createElement('link');
+        element.href = url;
+        element.rel = 'stylesheet';
+        element.type = 'text/css';
+        document.getElementsByTagName('head')[0].appendChild(element)
+    
+    }
+    
+    importCSS( 'http://fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' );
+    importCSS( 'rstats.css' );
 
     var _settings = settings || {},
         _colours = [ '#850700', '#c74900', '#fcb300', '#284280', '#4c7c0c' ];
 
     if( !_settings.values ) _settings.values = {};
     
-    function Graph( _dom, _id ) {
+    function Graph( _dom, _id, _def ) {
 
+        var _def = _def || {};
         var _canvas = document.createElement( 'canvas' ),
             _ctx = _canvas.getContext( '2d' ),
             _max = 0,
             _current = 0;
 
+        var c = _def.color?_def.color:'#666666';
+        var wc = _def.warningColor?_def.warningColor:'#b70000';
+        
         var _dotCanvas = document.createElement( 'canvas' ),
             _dotCtx = _dotCanvas.getContext( '2d' );
         _dotCanvas.width = 1;
-        _dotCanvas.height = 20;
+        _dotCanvas.height = 2 * _elHeight;
         _dotCtx.fillStyle = '#444444';
-        _dotCtx.fillRect( 0, 0, 1, 20 );
-        _dotCtx.fillStyle = '#666666';
-        _dotCtx.fillRect( 0, 10, 1, 10 );
+        _dotCtx.fillRect( 0, 0, 1, 2 * _elHeight );
+        _dotCtx.fillStyle = c;
+        _dotCtx.fillRect( 0, _elHeight, 1, _elHeight );
         _dotCtx.fillStyle = '#ffffff';
-        _dotCtx.fillRect( 0, 10, 1, 1 );
+        _dotCtx.globalAlpha = .5;
+        _dotCtx.fillRect( 0, _elHeight, 1, 1 );
+        _dotCtx.globalAlpha = 1;
 
         var _alarmCanvas = document.createElement( 'canvas' ),
             _alarmCtx = _alarmCanvas.getContext( '2d' );
         _alarmCanvas.width = 1;
-        _alarmCanvas.height = 20;
+        _alarmCanvas.height = 2 * _elHeight;
         _alarmCtx.fillStyle = '#444444';
-        _alarmCtx.fillRect( 0, 0, 1, 20 );
+        _alarmCtx.fillRect( 0, 0, 1, 2 * _elHeight );
         _alarmCtx.fillStyle = '#b70000';
-        _alarmCtx.fillRect( 0, 10, 1, 10 );
+        _alarmCtx.fillRect( 0, _elHeight, 1, _elHeight );
+        _alarmCtx.globalAlpha = .5;
         _alarmCtx.fillStyle = '#ffffff';
-        _alarmCtx.fillRect( 0, 10, 1, 1 );
+        _alarmCtx.fillRect( 0, _elHeight, 1, 1 );
+        _alarmCtx.globalAlpha = 1;
 
         function _init() {
 
-            _canvas.width = 200;
-            _canvas.height = 10;
+            _canvas.width = _elWidth;
+            _canvas.height = _elHeight;
             _canvas.style.width = _canvas.width + 'px';
             _canvas.style.height = _canvas.height + 'px';
-            _canvas.style.position = 'absolute';
-            _canvas.style.right = 0;
+            _canvas.className = 'rs-canvas'; 
             _dom.appendChild( _canvas );
 
             _ctx.fillStyle = '#444444';
@@ -89,9 +103,9 @@ function rStats( settings ) {
             if( _current > _max ) _max = _current;
             _ctx.drawImage( _canvas, 1, 0, _canvas.width - 1, _canvas.height, 0, 0, _canvas.width - 1, _canvas.height );
             if( alarm ) {
-                _ctx.drawImage( _alarmCanvas, _canvas.width - 1, _canvas.height - _current * _canvas.height / _max - 10 );
+                _ctx.drawImage( _alarmCanvas, _canvas.width - 1, _canvas.height - _current * _canvas.height / _max - _elHeight );
             } else {
-                _ctx.drawImage( _dotCanvas, _canvas.width - 1, _canvas.height - _current * _canvas.height / _max - 10 );
+                _ctx.drawImage( _dotCanvas, _canvas.width - 1, _canvas.height - _current * _canvas.height / _max - _elHeight );
             }
         }
 
@@ -112,12 +126,11 @@ function rStats( settings ) {
 
         function _init() {
 
-            _canvas.width = 200;
-            _canvas.height = 10 * _num;
+            _canvas.width = _elWidth;
+            _canvas.height = _elHeight * _num;
             _canvas.style.width = _canvas.width + 'px';
             _canvas.style.height = _canvas.height + 'px';
-            _canvas.style.position = 'absolute';
-            _canvas.style.right = 0;
+            _canvas.className = 'rs-canvas'; 
             _dom.appendChild( _canvas );
 
             _ctx.fillStyle = '#444444';
@@ -150,29 +163,23 @@ function rStats( settings ) {
             _time,
             _value = 0,
             _total = 0,
+            _averageValue = 0,
+            _accumValue = 0,
+            _accumStart = Date.now(),
+            _accumSamples = 0,
             _dom = document.createElement( 'div' ),
             _spanId = document.createElement( 'span' ),
             _spanValue = document.createElement( 'div' ),
             _spanValueText = document.createTextNode( '' ),
-            _graph = new Graph( _dom, _id ),
-            _def = _settings?_settings.values[ _id.toLowerCase() ]:null;
+            _def = _settings?_settings.values[ _id.toLowerCase() ]:null,
+            _graph = new Graph( _dom, _id, _def );
 
-        _dom.style.position = 'relative';
-        _dom.style.margin = '2px 0';
-        _dom.style.height = '10px';
-
-        _spanId.style.position = 'absolute';
-        _spanId.style.left = 0;
-        _spanId.style.top = 0;
+        _dom.className = 'rs-counter-base';
+       
+        _spanId.className = 'rs-counter-id'
         _spanId.textContent = ( _def && _def.caption )?_def.caption:_id;
 
-        _spanValue.style.position = 'absolute';
-        _spanValue.style.right = '210px';
-        _spanValue.style.width = '3em';
-        _spanValue.style.height = '1em';
-        _spanValue.style.top = 0;
-        _spanValue.style.textAlign = 'right';
-
+        _spanValue.className = 'rs-counter-value';
         _spanValue.appendChild( _spanValueText );
         
         _dom.appendChild( _spanId );
@@ -182,6 +189,19 @@ function rStats( settings ) {
 
         _time = performance.now();
 
+        function _average( v ) {
+            if( _def && _def.average ) {
+                _accumValue += v;
+                _accumSamples++;
+                var t = Date.now();
+                if( t - _accumStart >= ( _def.avgMs || 1000 ) ) {
+                    _averageValue = _accumValue / _accumSamples;
+                    _accumValue = 0;
+                    _accumStart = t;
+                    _accumSamples = 0;
+                }
+            }
+        }
 
         function _start(){
             _time = performance.now();
@@ -189,6 +209,7 @@ function rStats( settings ) {
 
         function _end() {
             _value = performance.now() - _time;
+            _average( _value );
         }
 
         function _tick() {
@@ -197,7 +218,8 @@ function rStats( settings ) {
         }
 
         function _draw() {
-            _spanValueText.nodeValue = Math.round( _value * 100 ) / 100;
+            var v = ( _def && _def.average )?_averageValue:_value
+            _spanValueText.nodeValue = Math.round( v * 100 ) / 100;
             var a = ( _def && ( ( _def.below && _value < _def.below ) || ( _def.over && _value > _def.over ) ) );
             _graph.draw( _value, a );
             _dom.style.color = a?'#b70000':'#ffffff';
@@ -211,11 +233,13 @@ function rStats( settings ) {
                 _value = _total * 1000 / e;
                 _total = 0;
                 _time = t;
-            }
+                _average( _value );
+           }
         }
 
         function _set( v ) {
             _value = v;
+            _average( _value );
         }
 
         return {
@@ -245,8 +269,11 @@ function rStats( settings ) {
 
     }
 
-    var _div,
-        _height = null;
+    var _base,
+        _div,
+        _height = null,
+        _elHeight = 10,
+        _elWidth = 200;
 
     var _perfCounters = {},
         _samples = {};
@@ -292,18 +319,16 @@ function rStats( settings ) {
             _settings.plugins = {};
         }
 
+        _base = document.createElement( 'div' );
+        _base.className = 'rs-base';
         _div = document.createElement( 'div' );
-        _div.style.position = 'absolute';
-        _div.style.zIndex = 10000;
-        _div.style.padding = '10px';
-        _div.style.backgroundColor = '#222';
-        _div.style.fontSize = '10px';
-        _div.style.lineHeight = '1.2em';
-        _div.style.width = '350px';
-        _div.style.fontFamily = 'Roboto Condensed, tahoma, sans-serif';
-        _div.style.left = _div.style.top = 0;
-        _div.style.overflow = 'hidden';
-        document.body.appendChild( _div );
+        _div.className = 'rs-container';
+        _div.style.height = 'auto';
+        _base.appendChild( _div );
+        document.body.appendChild( _base );
+
+        var style = window.getComputedStyle( _base, null ).getPropertyValue( 'font-size' );
+        //_elHeight = parseFloat( style );
 
         if( !_settings ) return;
 
@@ -311,14 +336,14 @@ function rStats( settings ) {
             for( var j in _settings.groups ) {
                 var g = _settings.groups[ parseInt( j, 10 ) ];
                 var div = document.createElement( 'div' );
+                div.className = 'rs-group';
                 g.div = div;
                 var h1 = document.createElement( 'h1' );
                 h1.textContent = g.caption;
-                h1.style.margin = h1.style.padding = 0;
-                h1.style.marginBottom = '5px';
-                div.style.marginBottom = '10px';
-                h1.style.fontSize = '14px';
-                h1.style.color = '#fff'
+                h1.addEventListener( 'click', function( e ) {
+                    this.classList.toggle( 'hidden' );
+                    e.preventDefault();
+                }.bind( div ) );
                 _div.appendChild( h1 );
                 _div.appendChild( div );
             }
@@ -328,26 +353,20 @@ function rStats( settings ) {
             for( var j in _settings.fractions ) {
                 var f = _settings.fractions[ parseInt( j, 10 ) ];
                 var div = document.createElement( 'div' );
+                div.className = 'rs-fraction';
                 var legend = document.createElement( 'div' );
-                legend.style.position = 'absolute';
-                legend.style.lineHeight = '10px';
-
+                legend.className = 'rs-legend';
+                
                 var h = 0;
                 for( var k in _settings.fractions[ j ].steps ) {
                     var p = document.createElement( 'p' );
                     p.textContent = _settings.fractions[ j ].steps[ k ];
                     p.style.color = _colours[ h ];
-                    p.style.width = '120px';
-                    p.style.textAlign = 'right';
-                    p.style.margin = 0;
-                    p.style.padding = 0;
                     legend.appendChild( p );
                     h++;
                 }
                 div.appendChild( legend );
-                div.style.height = h * 10 + 'px';
-                div.style.position = 'relative';
-                div.style.marginBottom = '5px';
+                div.style.height = h * _elHeight + 'px';
                 f.div = div;
                 var graph = new StackGraph( div, h );
                 f.graph = graph;
@@ -386,10 +405,11 @@ function rStats( settings ) {
             }
         }
 
-        if( _height == null ) {
+        /*if( _height != _div.clientHeight ) {
             _height = _div.clientHeight;
-            _div.style.height = _height + 'px';
-        }
+            _base.style.height = _height + 2 * _elHeight + 'px';
+        console.log( _base.clientHeight );
+        }*/
 
     }
 
